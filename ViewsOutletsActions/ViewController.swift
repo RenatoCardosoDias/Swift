73699acb1983508408bf90a51fb90c2266c0ef9b
Renato Dias <renatocardosodias@gmail.com>
2022-08-30 05:43:25 -0300

    Criado uma UIView e preenchida com a cor laranja, criado um botão, criado uma label na View principal

--- ViewsOutletsActions/ViewController.swift
+++ ViewsOutletsActions/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
