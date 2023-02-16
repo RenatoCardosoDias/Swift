71211387ac3b82bae3005aeb5701b282eaa51040
Renato Dias <renatocardosodias@icloud.com>
2023-02-16 00:15:30 -0300

    criando um NavigationLink para controlar a navegação dentro da NavigationStack

--- NavigationStack/NavigationStack/ContentView.swift
+++ NavigationStack/NavigationStack/ContentView.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
 
-    var plataform: [Plataform] = [.init(name: "Xbox", imageName: "xbox.logo", color: .green),
+    var plataforms: [Plataform] = [.init(name: "Xbox", imageName: "xbox.logo", color: .green),
                                   .init(name: "Playstation", imageName: "play.loto", color: .indigo),
                                   .init(name: "PC", imageName: "pc", color: .pink),
                                   .init(name: "Moblie", imageName: "iphone", color: .mint)]
 
     var body: some View {
         NavigationStack{
-
+            List{
+                Section("Plataform") {
+                    ForEach(plataforms, id: \.name) { plataform in
+                        NavigationLink(value: plataform){
+                            Label(plataform.name, systemImage: plataform.imageName)
+                                .foregroundColor(plataform.color)
+                        }
+                    } //end ForEach
+                } //end Section
+            } //end List
+            .navigationTitle("Gaming")
         } //end NavigationStack
     } //end var body
 } //end struct
@@ -27,7 +37,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Plataform{
+struct Plataform: Hashable{
     let name: String
     let imageName: String
     let color: Color
