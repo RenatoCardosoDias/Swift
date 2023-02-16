b44212c52884b37215080fbdc28fc8d32f3a8084
Renato Dias <renatocardosodias@icloud.com>
2023-02-15 23:39:42 -0300

    salvando

---
+++ NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Renato on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
