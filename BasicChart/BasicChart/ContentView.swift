684af81624f1003d5b27b934f487aae43eba17a2
Renato Dias <renatocardosodias@icloud.com>
2023-02-17 18:38:37 -0300

    criando um array com os meses do ano e vamos iniciar com os dados, como ano, mes e dia e com a quantidade de visualizações

--- BasicChart/BasicChart/ContentView.swift
+++ BasicChart/BasicChart/ContentView.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    //criando um array com os meses do ano e vamos iniciar com os dados, como ano, mes e dia e com a quantidade de visualizações
+    let viewMonths: [ViewMonth] = [
+        .init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 55000),
+        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 89000),
+        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 79000),
+        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 130000),
+        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 90000),
+        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 88000),
+        .init(date: Date.from(year: 2023, month: 8, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 9, day: 1), viewCount: 74000),
+        .init(date: Date.from(year: 2023, month: 10, day: 1), viewCount: 99000),
+        .init(date: Date.from(year: 2023, month: 11, day: 1), viewCount: 110000),
+        .init(date: Date.from(year: 2023, month: 12, day: 1), viewCount: 94000),
+    ]
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -33,8 +50,8 @@ struct ViewMonth: Identifiable {
 }
 
 //criando dados ficticios
-//esta extensão nos permite criar uma Data para a ViewMonth, passando os dodos como ano, mes e dia 
-extension Data {
+//esta extensão nos permite criar uma Data para a ViewMonth, passando os dodos como ano, mes e dia
+extension Date {
     static func from(year: Int, month: Int, day: Int) -> Date {
         let components = DateComponents(year: year, month: month, day: day)
         return Calendar.current.date(from: components)!
