e2391a90519d896e519a9d69e9e0af78e7d623da
Renato Dias <renatocardosodias@icloud.com>
2023-02-17 18:01:02 -0300

    Projeto de Estudo básico de Chart

---
+++ BasicChart/BasicChart/BasicChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicChartApp.swift
+//  BasicChart
+//
+//  Created by Renato on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
