fa888f8534aff5e2a3cff65dc78eb361e87b5d3d
Renato Dias <renatocardosodias@icloud.com>
2023-02-20 12:34:10 -0300

    Criando projeto Single-Player

---
+++ SinglePlayerTitcTacToe/SinglePlayerTitcTacToe/SinglePlayerTitcTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglePlayerTitcTacToeApp.swift
+//  SinglePlayerTitcTacToe
+//
+//  Created by Renato on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SinglePlayerTitcTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
