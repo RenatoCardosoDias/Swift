fa4a1590d37ed618be0a141df5b50a8b473aaafb
Renato Dias <renatocardosodias@icloud.com>
2023-03-03 17:11:37 -0300

    criamos uma visualização chamada EmptyStateView, vamos utilizar quando não encontrar alguma musica ou iniciar a view

--- AppMusicSearchApp/AppMusicSearchApp/ContentView.swift
+++ AppMusicSearchApp/AppMusicSearchApp/ContentView.swift
@@ -9,15 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        NavigationView{
+            VStack{
+                EmptyStateView()
+            } //end VStack
+        } //end NaviagionView
+    } //end var body
+} //end struct
+
+struct EmptyStateView: View {
+
+    var body: some View{
+        VStack{
+            Spacer()
+            Image(systemName: "music.note")
+                .font(.system(size: 85))
+                .padding(.bottom)
+            Text("Stating searching for music...")
+                .font(.title)
+            Spacer()
+        } //end VStack
         .padding()
-    }
-}
+        .foregroundColor(Color(.systemIndigo))
+    } //end var body
+} //end struct EmptyStateView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
