8532e80115bbfb87a2ee573854dcfc6306e15bf9
Renato Dias <renatocardosodias@icloud.com>
2023-03-03 16:56:03 -0300

    Criando um projeto para procurar musicas usando api do iTunes

---
+++ AppMusicSearchApp/AppMusicSearchApp/AppMusicSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMusicSearchAppApp.swift
+//  AppMusicSearchApp
+//
+//  Created by Renato on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AppMusicSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
