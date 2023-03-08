8b1b229d61d24468012fad9a3c46c62955b682e6
Renato Dias <renatocardosodias@icloud.com>
2023-03-08 17:12:58 -0300

    adicionado um array com vários emoji, foi também criado um zstack para apresentar as imagens dos emojis e envolvido dentro de um Hstack para informar o nome da cada emoji

--- ListAndNavigation/ListAndNavigation/ContentView.swift
+++ ListAndNavigation/ListAndNavigation/ContentView.swift
@@ -3,20 +3,96 @@
 //  ListAndNavigation
 //
 //  Created by Renato on 08/03/23.
-//
+// mostrando como podemos usar uma lista em swiftUI, como podemos usar com NavigationLink para navegar entre uma view e outra em seu app
+
+//NavigationLink  - uma visualização que controla uma apresentação de navegação
 
 import SwiftUI
 
 struct ContentView: View {
+
+    private let emojiList: [EmojiItem] = [
+        EmojiItem(
+            emoji: "👾",
+            name: "Alien Monster",
+            description: "A friendly-looking, tentacled space creature with two eyes."),
+        EmojiItem(
+            emoji: "🥑",
+            name: "Avocado",
+            description: "A pear-shaped avocado, sliced in half to show its yellow-green flesh and "
+            + "large brown pit."),
+        EmojiItem(
+            emoji: "🍟",
+            name: "French Fries",
+            description: "Thin-cut, golden-brown French fries, served in a red carton, as at "
+            + "McDonald’s."),
+        EmojiItem(
+            emoji: "🍕",
+            name: "Pizza",
+            description: "A slice of pepperoni pizza, topped with black olives on Google. WhatsApp "
+            + "adds green pepper, Samsung white onion."),
+        EmojiItem(
+            emoji: "🧩",
+            name: "Puzzle Piece",
+            description: "Puzzle Piece was approved as part of Unicode 11.0 in 2018 under the name "
+            + "“Jigsaw Puzzle Piece” and added to Emoji 11.0 in 2018."),
+        EmojiItem(
+            emoji: "🚀",
+            name: "Rocket",
+            description: "A rocket being propelled into space."),
+        EmojiItem(
+            emoji: "🗽",
+            name: "Statue of Liberty",
+            description: "The Statue of Liberty, often used as a depiction of New York City."),
+        EmojiItem(
+            emoji: "🧸",
+            name: "Teddy Bear",
+            description: "A classic teddy bear, as snuggled by a child when going to sleep."),
+        EmojiItem(
+            emoji: "🦄",
+            name: "Unicorn",
+            description: "The face of a unicorn, a mythical creature in the form of a white horse with "
+            + "a single, long horn on its forehead."),
+        EmojiItem(
+            emoji: "👩🏽‍💻",
+            name: "Woman Technologist",
+            description: "A woman behind a computer screen, working in the field of technology."),
+        EmojiItem(
+            emoji: "🗺",
+            name: "World Map",
+            description: "A rectangular map of the world. Generally depicted as a paper map creased at "
+            + "its folds, Earth’s surface shown in green on blue ocean."),
+    ]
+
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        NavigationView{
+            List(emojiList) { emojiItem in
+                HStack {
+                    ZStack {
+                        Text(emojiItem.emoji)
+                            .shadow(radius: 3)
+                            .font(.largeTitle)
+                            .frame(width: 65, height: 65)
+                            .overlay(
+                                Circle().stroke(Color.purple, lineWidth: 3)
+                            ) //end .overlay
+                    } //end ZStack
+                    Text(emojiItem.name)
+                        .font(.headline)
+                } //end HStack
+                .padding(7)
+            } //end List
+            .navigationTitle("Emoji")
+        } //end NavigationView
+    } //end var body
+} //end struct Content View
+
+struct EmojiItem: Identifiable {
+    let id = UUID()
+    let emoji: String
+    let name : String
+    let description: String
 }
 
 struct ContentView_Previews: PreviewProvider {
