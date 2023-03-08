c6e95ca6d9a3feb9e3c2b73b30bac0563cc2d08d
Renato Dias <renatocardosodias@icloud.com>
2023-03-08 16:26:54 -0300

    SwiftUI Tutorial - Lista and Navigation

---
+++ ListAndNavigation/ListAndNavigation/ListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndNavigationApp.swift
+//  ListAndNavigation
+//
+//  Created by Renato on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
