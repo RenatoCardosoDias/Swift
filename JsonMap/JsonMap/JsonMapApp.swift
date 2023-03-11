06782baf0a1bd331b4f23cadb81d6b71a6c5df35
Renato Dias <renatocardosodias@icloud.com>
2023-03-10 22:43:09 -0300

    Como ler um arquivo Json

---
+++ JsonMap/JsonMap/JsonMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonMapApp.swift
+//  JsonMap
+//
+//  Created by Renato on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct JsonMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
